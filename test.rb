e214c75fabcfada9c80d05f0a98bc95665885c6a
testmore <user@Users-MacBook-Air.local>
2015-06-24 21:59:04 +0100

    add test.rb

---
+++ test.rb
@@ -0,0 +1,11 @@
+def solution(str)
+  #-- get the length of the strin
+  stringLength = str.length
+  #-- if not even legth string then pad end with an underscore "_"
+  if stringLength % 2 != 0 then
+    str << "_"
+  end
+  #Create Array and split string into pairs of two characters
+  arr = []
+  arr << str.chars.each_slice(2).map(&:join)
+end
